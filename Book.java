910ee1334ff4b269996bbfe5d5e86005034f7f15
raviva07 <ravivarma7939@gmail.com>
2026-01-20 12:12:27 +0530

    Book class

---
+++ Book.java
@@ -0,0 +1,29 @@
+class Book {
+    private int bookID;
+    private String title;
+    private String author;
+    private boolean isAvailable;
+
+    public Book(int bookID, String title, String author, boolean isAvailable) {
+        this.bookID = bookID;
+        this.title = title;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String toString() {
+        return bookID + " | " + title + " | " + author + " | " + isAvailable;
+    }
+}
