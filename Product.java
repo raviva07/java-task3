ca00cce13979c9b74ad5b32c91b265dab23d411c
raviva07 <ravivarma7939@gmail.com>
2026-01-20 12:32:18 +0530

    Product class to Implement Taxable Interface

---
+++ Product.java
@@ -0,0 +1,15 @@
+class Product implements Taxable {
+    private int pid;
+    private double price;
+    private int quantity;
+
+    public Product(int pid, double price, int quantity) {
+        this.pid = pid;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double calcTax() {
+        return price * salesTax;
+    }
+}
